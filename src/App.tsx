f2a0c3565a213aceba92d0cca10f4817293d1497
reghev <theragsgg@gmail.com>
2025-12-24 10:04:02 -0500

    more vercel stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import ProjectLois from './Pages/mark-wahlberg'
-import ProjectCircuit from './Pages/takeachillpill'
-import ProjectDinosaur from './Pages/seth-green'
-import IceCream from './Pages/icecreamtierlist'
-import ProteinPowder from './Pages/proteinpowdertierlist'
-import EnergyDrinks from './Pages/energydrinkstierlist'
-import Shhh from './Pages/shhh'
-import Fuzzgenes from './Pages/Fuzzgenes'
-import Dexterio from './Pages/dexterio'
-import Clash from './Pages/clash'
-import GuestBook from './Pages/guestbook'
-import Projects from './Pages/projects'
-console.log(Home, About)
+import Home from './pages/Home'
+import About from './pages/About'
+import ProjectLois from './pages/mark-wahlberg'
+import ProjectCircuit from './pages/takeachillpill'
+import ProjectDinosaur from './pages/seth-green'
+import IceCream from './pages/icecreamtierlist'
+import ProteinPowder from './pages/proteinpowdertierlist'
+import EnergyDrinks from './pages/energydrinkstierlist'
+import Shhh from './pages/shhh'
+import Fuzzgenes from './pages/Fuzzgenes'
+import Dexterio from './pages/dexterio'
+import Clash from './pages/clash'
+import GuestBook from './pages/guestbook'
+import Projects from './pages/projects'
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
+        <Route path="/bout" element={<About />} />
         <Route path="/project/pathfinder" element={<ProjectLois />} />
         <Route path="/project/reality" element={<ProjectCircuit />} />
         <Route path="/project/solder" element={<ProjectDinosaur />} />
